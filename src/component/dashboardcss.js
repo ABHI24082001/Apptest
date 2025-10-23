ad7109b6cae439da882b568254ab08240f9b4819
Abhishek Kumar <sonukr2082001@gmail.com>
2025-10-23 15:32:40 +0530

    push

--- src/component/dashboardcss.js
+++ src/component/dashboardcss.js
@@ -5,7 +5,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#F9FAFB',
     paddingTop: Platform.OS === 'ios' ? 44 : 0,
-    marginBottom: -40, // Added bottom padding to prevent content from being cut off
+    marginBottom: -10, // Added bottom padding to prevent content from being cut off
   },
   header: {
     paddingHorizontal: 20,
--- src/constants/apiConfig.js
+++ src/constants/apiConfig.js
@@ -1,5 +1,5 @@
-const BASE_URL = 'http://192.168.29.2:91/api';
-// const BASE_URL = 'https://hcmapiv2.anantatek.com/api';
+// const BASE_URL = 'http://192.168.29.2:91/api';
+const BASE_URL = 'https://hcmapiv2.anantatek.com/api';
 export default BASE_URL;
 
 
